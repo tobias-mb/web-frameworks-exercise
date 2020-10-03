ee3e49cc1a400c1f08fd34f9b559b2660bd9bb1e
tobias-mb <tobias.mb@gmx.de>
2020-10-03 13:30:54 +0200

    added charger data

--- car-charger-app/src/components/ListElement.js
+++ car-charger-app/src/components/ListElement.js
@@ -4,11 +4,12 @@ import styles from './ListElement.module.css'
 export default function ListElement(props) {
     return (
         <li className = {styles.listElement} onClick={ () => props.flipDetailView(props.id) }>
-            <div className = {styles.flex}>
-                <div className = {styles.name}> {props.name} </div>
-                <div className = {styles.type}> {props.type} </div>
-            </div>
+            <div className = {styles.name}> {props.name} </div>
             <div> {props.address} </div>
+            <div style = {{display: 'flex'}} >
+                <div className = {styles.type}> {props.type} </div>
+                <div> - {props.power}kW </div>
+            </div>
         </li>
     )
 }
