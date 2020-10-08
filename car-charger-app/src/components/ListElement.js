d82f26dca05c115516932f7a87983e26e2d19d64
tobias-mb <tobias.mb@gmx.de>
2020-10-08 16:14:35 +0200

    add ConnectionType to ListElement also

--- car-charger-app/src/components/ListElement.js
+++ car-charger-app/src/components/ListElement.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import styles from './ListElement.module.css'
+import ConnectionType from './ConnectionType'
 
 export default function ListElement(props) {
     return (
         <li className = {styles.listElement} onClick={ () => props.flipDetailView(props.id) }>
             <div className = {styles.name}> {props.name} </div>
             <div> {props.address} </div>
-            <div style = {{display: 'flex'}} >
-                <div className = {styles.type}> {props.connections[0].type} </div>
-                <div> - {props.connections[0].powerKw}kW </div>
-                <div> &nbsp; &nbsp; {props.connections[0].available}/{props.connections[0].maxAvailable} free </div>
-            </div>
+            {props.connections.map(connection => <ConnectionType {...connection} key = {connection.id} />) }
         </li>
     )
 }
