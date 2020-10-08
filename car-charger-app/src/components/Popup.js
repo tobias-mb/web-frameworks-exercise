dad5ca532decc36ec90331f2eb762d35a51771e2
tobias-mb <tobias.mb@gmx.de>
2020-10-08 22:47:04 +0200

    log in with enter

--- car-charger-app/src/components/Popup.js
+++ car-charger-app/src/components/Popup.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 //Username and Password field of the Log In
 export default function Popup(props) {
+
+    const onEnter = (e) => {
+        props.authUser(props.usernameString, props.passwordString);
+        e.preventDefault();
+    }
+
     return (
         <div >
             <div style = {{display: 'flex'}} >
@@ -9,14 +15,14 @@ export default function Popup(props) {
                 <button onClick={() => props.authUser(props.usernameString, props.passwordString) }> Log In </button>
                 <button onClick={() => props.registerUser(props.usernameString, props.passwordString) }> Register </button>
             </div>
-            <div style={{display: 'flex'}} >
+            <form style={{display: 'flex'}} onSubmit={onEnter} >
                 <div>Username: &nbsp; </div>
-                <input type = "text" onChange ={ props.onUsernameFieldChange } value={ props.usernameString } />
-            </div>
-            <div style={{display: 'flex'}} >
+                <input type = "text" onChange ={ props.onUsernameFieldChange } value={ props.usernameString }/>
+            </form>
+            <form style={{display: 'flex'}} onSubmit={onEnter} >
                 <div>Password: &nbsp;&nbsp; </div>
                 <input type = "text" onChange ={ props.onPasswordFieldChange } value={ props.passwordString } />
-            </div>
+            </form>
             <div> &nbsp; </div>
         </div>
     )
